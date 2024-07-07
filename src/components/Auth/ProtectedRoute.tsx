8f56bb11a78d5f036e530b8a0b4d72d169067cb6
Tarangladumor <tarangladumor@gmail.com>
2024-07-07 17:32:47 +0530

    updated

--- src/components/Auth/ProtectedRoute.tsx
+++ src/components/Auth/ProtectedRoute.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 interface ProtectedRouteProps {
     children: React.ReactNode;
 }
 
+const navigate = useNavigate();
+
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
     const userInfo = localStorage.getItem('userInfo');
     
     if (!userInfo) {
-        return <Navigate to="/" state={{ error: 'Please fill out the form before accessing this page.' }} />;
+        alert('You must enter your details before accessing this page.');
+        navigate("/");
     }
 
     return <>{children}</>;
